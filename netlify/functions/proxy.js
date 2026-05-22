0ba09e3d557f0028153493b2361c71e8e65ef9fc
m-kozima <m-kozima@nice-nail.com>
2026-05-22 14:40:24 +0900

    fix proxy redirect

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,37 +1,47 @@
 const https = require('https');
 
+function followRedirects(url, method, body, callback) {
+  const urlObj = new URL(url);
+  const options = {
+    hostname: urlObj.hostname,
+    path: urlObj.pathname + urlObj.search,
+    method: method || 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  };
+  const req = https.request(options, (res) => {
+    if (res.statusCode === 301 || res.statusCode === 302 || res.statusCode === 307) {
+      followRedirects(res.headers.location, method, body, callback);
+      return;
+    }
+    let data = '';
+    res.on('data', chunk => data += chunk);
+    res.on('end', () => callback(null, data));
+  });
+  req.on('error', (e) => callback(e));
+  if (body) req.write(body);
+  req.end();
+}
+
 exports.handler = async function(event) {
   const GAS_URL = 'https://script.google.com/macros/s/AKfycbxlhZpXIi1Yl2Yj6BRUq4i99P_4TRYKJiF9hBxVRB2fEjb3I44pG9Mf4sXE9YxkazA/exec';
-  
   const params = event.queryStringParameters || {};
-  const queryString = Object.keys(params).map(k => k + '=' + params[k]).join('&');
+  const queryString = Object.keys(params).map(k => k + '=' + encodeURIComponent(params[k])).join('&');
   const url = GAS_URL + (queryString ? '?' + queryString : '');
 
-  if (event.httpMethod === 'POST') {
-    return new Promise((resolve) => {
-      const req = https.request(url, { method: 'POST' }, (res) => {
-        let data = '';
-        res.on('data', chunk => data += chunk);
-        res.on('end', () => resolve({
-          statusCode: 200,
-          headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-          body: data
-        }));
-      });
-      req.write(event.body || '');
-      req.end();
-    });
-  }
-
   return new Promise((resolve) => {
-    https.get(url, (res) => {
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => resolve({
+    followRedirects(url, event.httpMethod, event.body, (err, data) => {
+      if (err) {
+        resolve({ statusCode: 500, body: JSON.stringify({ error: err.message }) });
+        return;
+      }
+      resolve({
         statusCode: 200,
-        headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
         body: data
-      }));
+      });
     });
   });
 };
